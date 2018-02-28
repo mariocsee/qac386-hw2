fa2c44b663100f66dde070ddc472b3f37e704f06
Mario See <mariocsee@users.noreply.github.com>
2018-02-27 20:20:05 -0500

    donezos

--- hw2.R
+++ hw2.R
@@ -5,46 +5,53 @@ t = paste(a, collapse=" ")
 t = gsub("[^-A-Za-z., :!?]", " ", t) # Pavel cleaned it up. Remove any character that's not these
 t = gsub("[ ]{2,}", " ", t) #If space two times or more in a row, remove extra spaces
 
+install.packages("tm")
+library(tm)
+install.packages("descr")
+library(descr)
 # possibly cut up t
 
 places = c("Constantinople", "Egypt", "Alexandria", "Persia")
 
-patterns = list()
-for (i in 1:(length(places))) {
-  p = paste(" ([a-z]+ ){2}(?=", places[i] ,"[^a-z])", sep="")  
-  m = gregexpr(p, t, perl = T, useBytes = T)
-  v = regmatches(t, m)[[1]]
+generate_patterns_places <- function(sample) {
   
-  # find top frequency
-  # find unique top 20
+  patterns = list()
   
-  patterns[[(length(patterns)+1):(length(patterns)+1)]] = v[1:20]
-}
-
-patterns
-# clean it up
-# prefer patterns that are like 2/3 words 
-# 20 patterns
-
-patterns[[4]][20]
-
-final_places = list()
-for (i in 1:4) {
-  for (j in 1:20) {
-    print(i, j)
-    p_temp = paste("(?<=", patterns[[i]][j] ,")","([A-Z][a-z]+[ ]*)+", sep="")
-    print(p_temp)
-    m = gregexpr(p_temp, t, perl = T)
-    v2 = regmatches(t, m)[[1]]
-    # unique
-    # run it against the getty thing and work with the XML to get the subject_ID
+  for (i in 1:(length(places))) {
+    p = paste(" ([a-z]+ ){2}(?=", sample[i] ,"[^a-z])", sep="")  
+    m = gregexpr(p, t, perl = T, useBytes = T)
+    v = regmatches(t, m)[[1]]
     
-    final_places = append(final_places, v2)
+    
+    v = sort(table(v),decreasing=TRUE)[1:20]
+    v = as.data.frame(v)
+    v$v[1:20]
+    
+    patterns[[(length(patterns)+1):(length(patterns)+1)]] = v$v
   }
+  
+  final_places = list()
+  for (i in 1:4) {
+    for (j in 1:20) {
+      p_temp = paste("(?<=", patterns[[i]][j] ,")","([A-Z][a-z]+[ ]*)+", sep="")
+      m = gregexpr(p_temp, t, perl = T)
+      v2 = regmatches(t, m)[[1]]
+      v2
+      final_places = append(final_places, v2)
+    }
+  }
+  
+  return(final_places)
 }
 
+final_places = generate_patterns_places(places)
 final_places
 
+new_sample = sample(unique(final_places), 10)
+new_sample
 
 
-# stopped work here
+round2 = generate_patterns_places(new_sample)
+round2
+
+unique(round2)
